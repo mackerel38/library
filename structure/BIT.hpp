953adee47faa974b53adfc98d8861e10345c80fb
poe <mackerel38383838@gmail.com>
2025-03-03 15:24:34 +0900

    BITなおした

--- structure/BIT.hpp
+++ structure/BIT.hpp
@@ -1,16 +1,10 @@
 #line 1 "structure/BIT.hpp"
-template <typename T>
+template <class T>
 struct BIT {
     int n;
     vector<T> data;
-    BIT(int _n) : n(_n) {
-        data.assign(_n+1, T{});
-    }
-    BIT(vector<T>& v) : n((int)size(v)) {
-        build(v);
-    }
-    void build(vector<T>& v) {
-        assert(n == (int)size(v));
+    BIT(int _n) : n(_n), data(_n+1, T{}) {}
+    BIT(vector<T>& v) : n((int)size(v)), data((int)size(v)+1, T{}) {
         for (int i=1; i<=n; i++) {
             data[i] = v[i-1];
         }
@@ -30,6 +24,12 @@ struct BIT {
     void set(int p, T x) {
         add(int p, x - (sum(x+1) - sum(x)));
     }
+    void imos(int l, int r, T x) {
+        add(l, x);
+        if (r < n) {
+            add(r, -x);
+        }
+    }
     T sum(int p) {
         assert(0<=p && p<=n);
         T re = T{};
@@ -40,6 +40,9 @@ struct BIT {
     }
     T sum(int l, int r) {
         return sum(r) - sum(l);
+    }
+    T all_sum() {
+        return sum(n);
     }
     T operator[](int p) { return sum(p+1) - sum(p); }
 };
