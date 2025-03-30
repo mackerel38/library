e0cfbbbb25136aa9d4f8c8d473c97154787f6776
mackerel38 <mackerel38383838@gmail.com>
2025-03-31 04:57:32 +0900

    sparsetable の実装

---
+++ structure/sparsetable.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include<bits/stdc++.h>
+using namespace std;
+template<class T, auto op=min<T>>
+struct sparsetable {
+    int n;
+    vector<vector<T>> data;
+    // 0-indexed で構築する O(n log n)
+    sparsetable(const vector<T>& v) : n(v.size()) {
+        int m = 1;
+        while (m < n) m <<= 1;
+        data.assign(m, vector<T>(__lg(m)+1));
+        for (int i = 0; i < n; i++) data[i][0] = v[i];
+        for (int j = 1; j <= __lg(m); j++) {
+            for (int i = 0; i + (1 << j) <= n; i++) {
+                data[i][j] = op(data[i][j-1], data[i+(1<<(j-1))][j-1]);
+            }
+        }
+    }
+    // [l, r) の値を取得する O(1)
+    T query(int l, int r) {
+        assert(0 <= l && l <= r && r <= n);
+        if (l == r) return T{};
+        int j = __lg(r-l);
+        return op(data[l][j], data[r-(1 << j)][j]);
+    }
+};
