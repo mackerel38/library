07dc94b9ae2aba3bcf56b8c231bd99df878e25ea
mackerel38 <mackerel38383838@gmail.com>
2025-03-31 16:16:35 +0900

    template の追加

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "template"
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout<<fixed<<setprecision(30);
+    int T=1;
+    // cin >> T;
+    while (T--) solve();
+}
+
+void solve() {
+}
